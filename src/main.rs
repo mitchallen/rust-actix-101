2e3c18eb4edc681678b9770cda07f10e69c6501a
Mitch Allen <github@mitchallen.com>
2024-03-30 16:17:28 -0400

    feat: refactored for better performance

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,57 @@
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
 
-async fn greet() -> impl Responder {
-    HttpResponse::Ok().body("Hello, Actix!")
+#[get("/hello/{name}")]
+async fn greet(name: web::Path<String>) -> impl Responder {
+    format!("Hello {name}!")
 }
 
-#[actix_web::main]
+// async fn greet() -> impl Responder {
+//     HttpResponse::Ok().body("Hello, Actix!")
+// }
+
+#[get("/ping")]
+async fn ping() -> impl Responder {
+    HttpResponse::Ok().body("pong")
+}
+
+#[actix_web::main] // or #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().route("/", web::get().to(greet)))
-        .bind("127.0.0.1:8080")?
-        .run()
-        .await
+    HttpServer::new(|| {
+        // App::new().service(greet)
+        App::new().service(ping)
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
 
 #[cfg(test)]
 mod tests {
-    // use super::*;
-    // use actix_web::{test, App};
+    use actix_web::{test, App};
+    use actix_web::web::ServiceConfig;
+    use super::*;
+
+    // Function to configure services for the test app
+    // It mirrors the configuration of the actual app
+    fn test_config(cfg: &mut ServiceConfig) {
+        cfg.service(ping);
+    }
 
     #[actix_rt::test]
-    async fn test_example() {
-        // Your async test code here
+    async fn test_ping_route() {
+        // Setup test server with the ping service
+        let mut app = test::init_service(App::new().configure(test_config)).await;
+        // Create a request to the "/ping" endpoint
+        let req = test::TestRequest::get().uri("/ping").to_request();
+        // Execute request
+        let resp = test::call_service(&mut app, req).await;
+        
+        // Check if the response status is 200
+        assert!(resp.status().is_success());
+        // Extract response body
+        let body = test::read_body(resp).await;
+        // Convert body to string and compare
+        assert_eq!(body, "pong");
     }
 }
+
